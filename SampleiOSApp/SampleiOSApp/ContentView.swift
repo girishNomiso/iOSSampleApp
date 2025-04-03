f331b392ce29cef494127d223b53a88a66752658
Girish <girishc@cliqbux.com>
2025-04-03 16:26:30 +0530

    Old version app: SampleiOSApp

---
+++ SampleiOSApp/SampleiOSApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleiOSApp
+//
+//  Created by Girish on 03/04/25.
+//  Copyright © 2025 Girish. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
